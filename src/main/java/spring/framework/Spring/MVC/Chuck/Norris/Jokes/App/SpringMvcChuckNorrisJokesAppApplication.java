ab3ace876bbe6c6315951fc2c0904822a9da504f
KarchevBorislav <bkarchev@web.de>
2024-09-17 13:21:35 +0200

    Initiate  Project

---
+++ src/main/java/spring/framework/Spring/MVC/Chuck/Norris/Jokes/App/SpringMvcChuckNorrisJokesAppApplication.java
@@ -0,0 +1,13 @@
+package spring.framework.Spring.MVC.Chuck.Norris.Jokes.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcChuckNorrisJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcChuckNorrisJokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/framework/Spring/MVC/Chuck/Norris/Jokes/App/SpringMvcChuckNorrisJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.framework.Spring.MVC.Chuck.Norris.Jokes.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcChuckNorrisJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
