eb33a82bc74b59fd9c98155d3377d845791cb6ff
KarchevBorislav <bkarchev@web.de>
2024-09-17 13:40:43 +0200

    Adding Services,Controllers and Thymeleaf

---
+++ src/main/java/spring/framework/Spring/MVC/Chuck/Norris/Jokes/App/controllers/JokeController.java
@@ -0,0 +1,30 @@
+package spring.framework.Spring.MVC.Chuck.Norris.Jokes.App.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring.framework.Spring.MVC.Chuck.Norris.Jokes.App.service.JokeService;
+
+@Controller
+public class JokeController {
+
+ private final JokeService jokeService;
+
+
+ @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+
+
+    @RequestMapping("/")
+    public String showJoke(Model model){
+     model.addAttribute("joke",jokeService.getJoke());
+
+     return  "joke";
+    }
+
+
+}
